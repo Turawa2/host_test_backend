4cfde599f06969d3a8ecc2192588273a6798670e
Turawa <hamzamusaturawa@gmail.com>
2024-02-29 10:36:05 +0100

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const express = require('express');
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
+const Pool = require('pg').Pool
+require('dotenv').config(); 
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING
 });
 
-
-
+pool.connect()
+    .then(() => console.log('PostgreSQL connected'))
+    .catch(err => console.error('PostgreSQL connection error', err));
 //trying to 
+
+
+// Define a POST endpoint to handle the incoming data
+// app.post('/submitData', (req, res) => {
+//   const { name, password } = req.body;
+
+//   // Validate if name and password are present
+//   if (!name || !password) {
+//     return res.status(400).json({ error: 'Name and password are required' });
+//   }
+
+//   // Insert data into the database
+//   const insertQuery = 'INSERT INTO test (username, password) VALUES (?, ?)';
+//   db.query(insertQuery, [name, password], (err, result) => {
+//     if (err) {
+//       console.error('Error inserting data into the database:', err);
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+
+//     const insertedData = {
+//       id: result.insertId,
+//       name: name,
+//       password: password,
+//     };
+
+//     console.log('Data inserted successfully:', insertedData);
+//     return res.status(200).json({ message: 'Data inserted successfully', data: insertedData });
+//   });
+// });
 
 
 // Define a POST endpoint to handle the incoming data
@@ -27,45 +55,61 @@ app.post('/submitData', (req, res) => {
 
   // Validate if name and password are present
   if (!name || !password) {
-    return res.status(400).json({ error: 'Name and password are required' });
+    return res.status(400).json({ error: 'Name and password are required' 
+});
   }
 
   // Insert data into the database
-  const insertQuery = 'INSERT INTO test (username, password) VALUES (?, ?)';
-  db.query(insertQuery, [name, password], (err, result) => {
+  const insertQuery = 'INSERT INTO users (username, password) VALUES ($1, $2)';
+  pool.query(insertQuery, [name, password], (err, result) => {
     if (err) {
       console.error('Error inserting data into the database:', err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
 
+    console.log(result)
+
     const insertedData = {
-      id: result.insertId,
       name: name,
       password: password,
     };
 
-    console.log('Data inserted successfully:', insertedData);
-    return res.status(200).json({ message: 'Data inserted successfully', data: insertedData });
+    // console.log('Data inserted successfully:', insertedData);
+    return res.status(200).json({ message: 'Data inserted successfully', 
+data: insertedData });
   });
 });
 
 // Define a GET endpoint to fetch all data from the database
+// app.get('/fetchData', (req, res) => {
+//   const selectQuery = 'SELECT * FROM test';
+//   db.query(selectQuery, (err, results) => {
+//     if (err) {
+//       console.error('Error getting data from the database:', err);
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+
+//     return res.status(200).json(results);
+//   });
+// });
+
+
+// Define a GET endpoint to fetch all data from the database
 app.get('/fetchData', (req, res) => {
-  const selectQuery = 'SELECT * FROM test';
-  db.query(selectQuery, (err, results) => {
+  const selectQuery = 'SELECT * FROM users';
+  pool.query(selectQuery, (err, results) => {
     if (err) {
-      console.error('Error fetching data from the database:', err);
+      console.error('Error getting data from the database:', err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
 
-    return res.status(200).json(results);
+    return res.status(200).json(results.rows);
   });
 });
 
 
 
-
-
+// starting the port on 5000
 
 app.listen(PORT, () => {
   console.log('App running on port', PORT);
